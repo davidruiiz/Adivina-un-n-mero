efa1e3d717d6968c199d2897f7b311d3e15ffa65
David Ruiz <113980722+davidruiiz@users.noreply.github.com>
2022-10-13 12:00:58 +0200

    Commit6

--- main.py
+++ main.py
@@ -6,19 +6,9 @@ i=0
 
 intento=int(input("Introduce un número:\n"))
 
-while True:
-    while intento<0 or intento>100:
-        intento=int(input("Introduce un número:\n"))
+def numero_intentos(num)
+def adivina_numero(num)
+if name==main:
+    print(número) 
+def dame_numero(num)
 
-    if intento==numero:
-        print("¡Ha acertado el número!")
-        break
-    elif intento>numero:
-        print("El numero es menor")
-    else:
-        print("El número es mayor")
-
-    i+=1
-    intento=101
-
-print("Se han necesitado",i,"intentos")
